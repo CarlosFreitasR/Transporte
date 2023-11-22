6abbb3d1e9393ec297e00c446881a8e21b51bdd8
carlos.fribeiro <carlos.ribeiro@mt.estudante.senai.br>
2023-11-21 21:15:06 -0400

    criacao get e set

--- src/Rota.java
+++ src/Rota.java
@@ -6,4 +6,44 @@ public class Rota {
     private float valorTotalViagem;
     private long tempoTotalViagem;
     private int numeroTrocasTransporte;
+
+    public List<Passagem> getPassagems() {
+        return passagems;
+    }
+
+    public void setPassagems(List<Passagem> passagems) {
+        this.passagems = passagems;
+    }
+
+    public boolean isIdaVolta() {
+        return idaVolta;
+    }
+
+    public void setIdaVolta(boolean idaVolta) {
+        this.idaVolta = idaVolta;
+    }
+
+    public float getValorTotalViagem() {
+        return valorTotalViagem;
+    }
+
+    public void setValorTotalViagem(float valorTotalViagem) {
+        this.valorTotalViagem = valorTotalViagem;
+    }
+
+    public long getTempoTotalViagem() {
+        return tempoTotalViagem;
+    }
+
+    public void setTempoTotalViagem(long tempoTotalViagem) {
+        this.tempoTotalViagem = tempoTotalViagem;
+    }
+
+    public int getNumeroTrocasTransporte() {
+        return numeroTrocasTransporte;
+    }
+
+    public void setNumeroTrocasTransporte(int numeroTrocasTransporte) {
+        this.numeroTrocasTransporte = numeroTrocasTransporte;
+    }
 }
