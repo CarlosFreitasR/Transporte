db790bb006cfd05be068ac1a65eaac9122629d18
EduardaDelgado13 <dudamaryya12@gmail.com>
2023-11-22 20:33:43 -0400

    modifiquei as classes onibus e trem

--- src/modelo/Onibus.java
+++ src/modelo/Onibus.java
@@ -8,47 +8,47 @@ public class Onibus extends Terrestre {
 
     @Override
     public void setCarga(float carga) {
-
+      this.carga = carga;
     }
 
     @Override
     public int getNumeroPassageros() {
-        return 0;
+        return numeroPassageros;
     }
 
     @Override
     public void setNumeroPassageros(int numeroPassageros) {
-
+     this.numeroPassageros = numeroPassageros;
     }
 
     @Override
     public String getModelo() {
-        return null;
+        return modelo;
     }
 
     @Override
     public void setModelo(String modelo) {
-
+     this.modelo = modelo;
     }
 
     @Override
     public String getCor() {
-        return null;
+        return cor;
     }
 
     @Override
     public void setCor(String cor) {
-
+        this.cor = cor;
     }
 
     @Override
     public float getVelociadade() {
-        return 0;
+        return velociadade;
     }
 
     @Override
     public void setVelociadade(float velociadade) {
-
+     this.velociadade = velociadade;
     }
 
     @Override
